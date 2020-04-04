a7a0d22f6988f0324d02f364621a26990a3a869b
John Hightshue <john.hightshue@gmail.com>
2020-04-04 15:01:52 -0700

    Set up environment variables

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const mongodb = require('mongodb')
 
-const connectionString = 'mongodb+srv://todoAppUser:todoAppPass@cluster0-m112t.mongodb.net/Poster?retryWrites=true&w=majority'
-
-mongodb.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
+mongodb.connect(process.env.CONNECTIONSTRING, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, client) {
     module.exports = client.db()
     const app = require('./app')
-    app.listen(3000)
+    app.listen(process.env.PORT)
 })
